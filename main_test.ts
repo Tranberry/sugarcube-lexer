d8bbef84712ceecfcd2efcfeddb43d3872d8e0ab
TRBRY <thissoda@gmail.com>
2024-10-08 01:34:05 +0200

    remove test for now

--- main_test.ts
+++ main_test.ts
@@ -1,34 +1,34 @@
-import { assertEquals } from "@std/assert";
-import { Tokenize } from "./lexer/lexer.ts";
-import {
-  General_TokenType,
-  SC_TokenType,
-  Twee_TokenType,
-} from "./tokens/TokenTypes.ts";
+// import { assertEquals } from "@std/assert";
+// import { Tokenize } from "./lexer/lexer.ts";
+// import {
+//   General_TokenType,
+//   SC_TokenType,
+//   Twee_TokenType,
+// } from "./tokens/TokenTypes.ts";
 
-type Tokens = General_TokenType | SC_TokenType | Twee_TokenType;
+// type Tokens = General_TokenType | SC_TokenType | Twee_TokenType;
 
-Deno.test(function tokenizeTest() {
-  const input = `:: StoryData`;
-  const tokens = Tokenize(input);
-  const END = {
-    token: { type: "EOF", value: "" },
-    start: input.length,
-    end: input.length,
-  };
+// Deno.test(function tokenizeTest() {
+//   const input = `:: StoryData`;
+//   const tokens = Tokenize(input);
+//   const END = {
+//     token: { type: "EOF", value: "" },
+//     start: input.length,
+//     end: input.length,
+//   };
 
-  assertEquals(
-    tokens,
-    [
-      {
-        token: {
-          type: "passage_start",
-          value: "::",
-        },
-        start: 0,
-        end: 1,
-      },
-      END,
-    ],
-  );
-});
+//   assertEquals(
+//     tokens,
+//     [
+//       {
+//         token: {
+//           type: "passage_start",
+//           value: "::",
+//         },
+//         start: 0,
+//         end: 1,
+//       },
+//       END,
+//     ],
+//   );
+// });
